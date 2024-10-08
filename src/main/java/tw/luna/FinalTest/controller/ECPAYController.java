941aa91373ead17ee5277318f630e341a77bb2f8
lunawewe <Ls22012900@gmail.com>
2024-10-08 22:38:35 +0800

    444

---
+++ src/main/java/tw/luna/FinalTest/controller/ECPAYController.java
@@ -0,0 +1,42 @@
+package tw.luna.FinalTest.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import tw.luna.FinalTest.dto.PostMerchantDto;
+import tw.luna.FinalTest.dto.QueryOrderDTO;
+import tw.luna.FinalTest.dto.orders.MerchantByUserDto;
+import tw.luna.FinalTest.service.ECPAYService;
+import tw.luna.FinalTest.service.OrderService;
+import tw.luna.FinalTest.service.OrdersService;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping ("/api/ECPAY")
+
+public class ECPAYController {
+
+    @Autowired
+    ECPAYService ecpayService;
+
+    @PostMapping("/ecpayCheckout")
+    public String ecpayCheckout(HttpServletResponse response,
+                                @RequestBody PostMerchantDto postMerchantDto) throws IOException {
+        String aioCheckOutALLForm = ecpayService.ecpayCheckout(postMerchantDto);
+        return aioCheckOutALLForm;
+    }
+
+    @PostMapping("/queryOrder")
+    public String queryOrder(HttpServletResponse response,
+                             @RequestBody QueryOrderDTO queryOrderDTO) throws IOException {
+        return ecpayService.queryOrder(queryOrderDTO);
+    }
+
+    @PutMapping("/changeOrderStatus")
+    public void changeOrderStatus(@RequestBody MerchantByUserDto merchantNo) {
+        ecpayService.changeOrderStatus(merchantNo.getMerchantNo());
+    }
+
+
+}
