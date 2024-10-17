751c669756dc78d3a6e7d0c5e7f1e2534357007f
lunawewe <Ls22012900@gmail.com>
2024-10-17 14:15:53 +0800

    嘗試修復綠界

--- src/main/java/integration/AllInOneBase.java
+++ src/main/java/integration/AllInOneBase.java
@@ -1,15 +1,10 @@
-package integration;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-
+import java.io.InputStream;
 import integration.ecpayOperator.EcpayFunction;
 import integration.errorMsg.ErrorMessage;
 import integration.exception.EcpayException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-
 
 public class AllInOneBase {
 	protected static String operatingMode;
@@ -29,50 +24,56 @@ public class AllInOneBase {
 	protected static String createServerOrderUrl;
 	protected static Document verifyDoc;
 	protected static String[] ignorePayment;
-	public AllInOneBase(){
-//		try{
+
+	public AllInOneBase() {
+		try {
 			Document doc;
-			/* when using web project*/
-//			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-//			String configPath = URLDecoder.decode(classLoader.getResource("/payment_conf.xml").getPath(), "UTF-8");
-//			doc = EcpayFunction.xmlParser(configPath);
-			/* when using testing code*/
-			String paymentConfPath = "./src/main/resources/payment_conf.xml";
-			doc = EcpayFunction.xmlParser(paymentConfPath);
-			
+
+			// 使用類路徑的方式讀取 XML 文件，以便在 JAR 和 Docker 中都能正確運行
+			try (InputStream paymentConfStream = getClass().getResourceAsStream("/payment_conf.xml")) {
+				if (paymentConfStream == null) {
+					throw new EcpayException("payment_conf.xml not found in the classpath");
+				}
+
+				// 使用 EcpayFunction 來解析 InputStream
+				doc = EcpayFunction.xmlParser(paymentConfStream);
+			}
+
+			// 設置文件結構並初始化配置
 			doc.getDocumentElement().normalize();
-			//OperatingMode
-			Element ele = (Element)doc.getElementsByTagName("OperatingMode").item(0);
+			Element ele = (Element) doc.getElementsByTagName("OperatingMode").item(0);
 			operatingMode = ele.getTextContent();
-			//MercProfile
-			ele = (Element)doc.getElementsByTagName("MercProfile").item(0);
+
+			ele = (Element) doc.getElementsByTagName("MercProfile").item(0);
 			mercProfile = ele.getTextContent();
-			//IsProjectContractor
-			ele = (Element)doc.getElementsByTagName("IsProjectContractor").item(0);
+
+			ele = (Element) doc.getElementsByTagName("IsProjectContractor").item(0);
 			isProjectContractor = ele.getTextContent();
-			//MID, HashKey, HashIV, PlatformID
+
 			NodeList nodeList = doc.getElementsByTagName("MInfo");
-			for(int i = 0; i < nodeList.getLength(); i++){
-				ele = (Element)nodeList.item(i);
-				if(ele.getAttribute("name").equalsIgnoreCase(mercProfile)){
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				ele = (Element) nodeList.item(i);
+				if (ele.getAttribute("name").equalsIgnoreCase(mercProfile)) {
 					MerchantID = ele.getElementsByTagName("MerchantID").item(0).getTextContent();
 					HashKey = ele.getElementsByTagName("HashKey").item(0).getTextContent();
 					HashIV = ele.getElementsByTagName("HashIV").item(0).getTextContent();
-					PlatformID = isProjectContractor.equalsIgnoreCase("N")? "" : MerchantID;
+					PlatformID = isProjectContractor.equalsIgnoreCase("N") ? "" : MerchantID;
 				}
 			}
-			// IgnorePayment
-			ele = (Element)doc.getElementsByTagName("IgnorePayment").item(0);
+
+			ele = (Element) doc.getElementsByTagName("IgnorePayment").item(0);
 			nodeList = ele.getElementsByTagName("Method");
 			ignorePayment = new String[nodeList.getLength()];
-			for(int i = 0; i < nodeList.getLength(); i++){
+			for (int i = 0; i < nodeList.getLength(); i++) {
 				ignorePayment[i] = nodeList.item(i).getTextContent();
 			}
-			if(HashKey == null){
+
+			if (HashKey == null) {
 				throw new EcpayException(ErrorMessage.MInfo_NOT_SETTING);
 			}
-//		} catch(UnsupportedEncodingException e){
-//			e.printStackTrace();
-//		}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new EcpayException("Failed to load configuration: " + e.getMessage());
+		}
 	}
 }
